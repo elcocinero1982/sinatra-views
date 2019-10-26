8be3eb83de3aec0b568608506730b6b78bd9aaef
jose luis <cortezluisjose@gmail.com>
2019-10-26 23:02:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,4 +5,24 @@ class App < Sinatra::Base
 	get '/' do
 		
 	end
+	 get '/' do
+      erb :index
+    end
+     get "/info" do
+      "Testing the info page"
+    end
+    get '/' do
+      erb :index
+    end
+ 
+    get "/info" do
+      erb :info
+    end
+     get '/' do
+      erb :index
+    end
+ 
+    get "/info" do
+      erb :dogs
+    end
 end
